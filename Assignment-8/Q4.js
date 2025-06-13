7a3938076677f3e6cd644904f61bedee7eaa5219
akhil-successiveTech <akhil.dhawan@successive.tech>
2025-06-13 17:03:42 +0530

    Revert "Feature/assignment 8"

--- Assignment-8/Q4.js
+++
@@ -1,8 +0,0 @@
-// There is no error in this code
-
-let myString = 'hello world';
-if (myString.length > 0) {
-    console.log('The length of the string is: ' + myString.length);
-} else {
-    console.log('The string is empty');
-}
